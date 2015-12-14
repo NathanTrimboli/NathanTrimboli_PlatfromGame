a60b468b54ea98d6ad79ba4fc4403dabd0608df2
NathanTrimboli <nathantrimboli@hotmail.com>
2015-12-14 05:12:56 -0500

    Many Changes
    Menu Navigation.
    Individual navigation screen controls.
    Level progression.
    Point system.
    Life system.
    Wolves and hit detection for both dog and sheep.
    Win and lose states.

--- code/game.js
+++ code/game.js
@@ -1,6 +1,13 @@
+	//Note sources
 	//http://eloquentjavascript.net/16_canvas.html
+
+// ***************************************Convert degrees to number based radians
+	function degreesToRadians(degrees) {
+		return (degrees * Math.PI)/180;
+	}
 	
-	// Create the canvas areas
+// ***************************************Create the canvas areas
+	
 	//Game Play Area
 	var canvas = document.getElementById('canvas');
 	var ctx = canvas.getContext('2d');
@@ -14,6 +21,7 @@
 	var ctx2 = canvas2.getContext('2d');
 	var ctx3= canvas2.getContext('2d');
 	var ctx4= canvas2.getContext('2d');
+	var ctx5 = canvas2.getContext('2d');
 	canvas2.width = window.innerWidth;
 	canvas2.height = window.innerHeight;
 	//Outermost Brown
@@ -22,15 +30,18 @@
 	//Inner Brown
 	ctx2.fillStyle = "SaddleBrown";
 	ctx2.fillRect(90,90,788,1046);
-	//Sky
-	var grd = ctx4.createLinearGradient(0,0,0,200);
-	grd.addColorStop(0,"#0000FF ");
-	grd.addColorStop(1,"#00BFFF");
-	ctx4.fillStyle = grd;
-	ctx4.fillRect(100,100,768,115);
-	
 	canvas2.style.left  = 0+'px';
 	canvas2.style.top  = 0+'px';
+	
+	//Sun Timer
+	ctx5.fillStyle = "yellow";
+	var radius = 50;
+	var x = 180; //+ xCircle;
+	var y = 15000; //+ yCircle;
+	ctx5.beginPath();
+	//Create circle
+	ctx5.arc(x, y, radius, 0, degreesToRadians(360), true);
+	ctx5.fill();
 	/*
 	//Text box screen Overlay
 	var canvas3 = document.getElementById('canvas3');
@@ -42,9 +53,6 @@
 	canvas3.style.top  = 0+'px';
 	canvas3.style.left  = 0+'px';
 	*/
-	function resizeCanvas() {
-		
-	}
 
 	// Level Background
 	var bgReady = false;
@@ -52,15 +60,6 @@
 	bgImage.onload = function () {
 		bgReady = true;
 	};
-	bgImage.src = "images/levels/Spring1.png";
-	
-	// Controls Background
-	var conReady = false;
-	var conImage = new Image();
-	conImage.onload = function () {
-		conReady = true;
-	};
-	conImage.src = "images/controls.png";
 
 	// dog image
 	var dogReady = false;
@@ -69,7 +68,7 @@
 		dogReady = true;
 	};
 	dogImage.src = "images/dog/DogR1.png";
-
+	
 	// sheep image
 	var sheepReady = false;
 	var sheepImage = new Image();
@@ -77,57 +76,198 @@
 		sheepReady = true;
 	};
 	sheepImage.src = "images/sheep/SheepR1.png";
+	
+	// wolf image
+	var wolfReady = false;
+	var wolfImage = new Image();
+	wolfImage.onload = function () {
+		wolfReady = true;
+	};
+	wolfImage.src = "images/wolf/WolfR1.png";
+	
 
-	// Game objects
+
+	
+// ***************************************Game objects Stats
 	var dog = {
-		speed: 256 // movement in pixels per second
+		speed: 256
 	};
 	var sheep = {
-		speed: 150 // movement in pixels per second
+		speed: 150
 	};
+	var wolf = {
+		speed: 120
+	};
+	
 	var sheepsCaught = 0;
+	var hitCount = 0;
 
-	// Handle keyboard controls
+// ***************************************Handle keyboard controls 
 	var keysDown = {};
-
 	addEventListener("keydown", function (e) {
 		keysDown[e.keyCode] = true;
 	}, false);
-
 	addEventListener("keyup", function (e) {
 		delete keysDown[e.keyCode];
 	}, false);
 
-	// Reset the game when the player catches a sheep
-	var first = true;
-	var reset = function () {
-	if (first){
-		dog.x = (canvas.width / 2)-dogImage.width;
-		dog.y = (canvas.height / 2)-dogImage.height;
-		first = false;
+	
+
+// ***************************************Reset the game when the player catches a sheep, wins, or loses
+var first = true;
+var wolfFirst = true;
+var menuScreen = true;
+var winLose = false;
+var wolfDog = false;
+
+//var wolfSheep = false;
+var gameLevel = 1;
+var controlSet = 1;
+
+
+var reset = function () {
+	
+// ***************************************Check to see if player is moving to next level
+	if (sheepsCaught == 500){
+		gameLevel += 1;
+		first = true;
+		if (gameLevel == 6){
+			winLose = true;
+		}
 	}
-	else{
-		dog.x = dog.x;
-		dog.y = dog.y;
+	if (hitCount == 3){
+		hitCount =0;
+		gameLevel = 7;
+		winLose = true;
+		first = true;
 	}
 	
-	//Create sheeps from array
-	//for (i=0; i < 5; i++){
-		// Throw the sheep somewhere on the screen randomly
-		sheep.x = 30 + Math.floor(Math.random() * (canvas.width - 80));
-		sheep.y = 220 + Math.floor(Math.random() * (canvas.height - 300));
-		console.log(sheep.y);
-		var monX = Math.floor(Math.random() * (2));
-		console.log(monX + ' :MONX');
-		sheepImage.src = "images/sheep/SheepR1.png";
-		if (monX == 0){
-			sheep.speed = -sheep.speed;
-			sheepImage.src = "images/sheep/SheepL1.png";
+// ***************************************Control Screen and game navigation and controls CONTROLS #0-2 / GAME LEVEL #0-2
+	if (menuScreen){
+		//Credits Page
+		if (gameLevel == 0){
+			bgImage.src = "images/levels/controls1.png";
+			controlSet = 0;
 		}
-	//}
-	};
+		//Main Menu
+		else if (gameLevel == 1){
+			bgImage.src = "images/levels/main.png";
+			controlSet = 1;
+		}
+		//Rules Page 1
+		else if (gameLevel == 2){
+			bgImage.src = "images/levels/rules1.png";
+			controlSet = 2;
+		}
+	}
+// ***************************************Level Screens CONTROL #3 / GAME LEVEL #3-5
+	if (!menuScreen){
+		//Sky
+		var grd = ctx4.createLinearGradient(0,0,0,200);
+		grd.addColorStop(0,"#0000FF ");
+		grd.addColorStop(1,"#00BFFF");
+		ctx4.fillStyle = grd;
+		ctx4.fillRect(100,100,768,115);
+	
+		controlSet = 3;
+		if (first){
+			//Level Spring
+			if (gameLevel == 3){
+				console.log('spring');
+				bgImage.src = "images/levels/Spring1.png";
+				sheepsCaught = 0;
+				hitCount = 0;
+			}
+			//Level Fall
+			else if (gameLevel == 4){
+				console.log('fall');
+				bgImage.src = "images/levels/Spring1.png";
+				sheepsCaught = 0;
+				hitCount = 0;
+			}
+			//Level Winter
+			else if (gameLevel == 5){
+				console.log('winter');
+				bgImage.src = "images/levels/Spring1.png";
+				sheepsCaught = 0;
+				hitCount = 0;
+			}
+		}
+	}
+// ***************************************Win Lose States CONTROL #4 / GAME LEVEL #6-7
+	if (winLose){
+		controlSet = 4;
+		//Level Win
+		if (gameLevel == 6){
+			bgImage.src = "images/levels/win.png";
+			sheepsCaught = 0;
+			hitCount = 0;
+			sheep.y = dog.y + 50;
+		}
+		//Level Lose
+		else if (gameLevel == 7){
+			bgImage.src = "images/levels/lose.png";
+			sheepsCaught = 0;
+			hitCount = 0;
+			sheep.y = dog.y + 50;
+		}
+	}
+// ***************************************Character placement upon resets
+	if (!menuScreen){
+		if(!winLose){
+			if (first){
+				dog.x = (canvas.width / 2)-dogImage.width;
+				dog.y = (canvas.height / 2)-dogImage.height;
+			}
+			if (wolfDog){
+				dog.x = (canvas.width / 2)-dogImage.width;
+				dog.y = (canvas.height / 2)-dogImage.height;
+			}
+			else{
+				dog.x = dog.x;
+				dog.y = dog.y;
+			}
+			//Create sheeps from array
+			//for (i=0; i < 5; i++){
+				if(!wolfDog){
+					// Throw the sheep somewhere on the screen randomly
+					sheep.x = 30 + Math.floor(Math.random() * (canvas.width - 80));
+					sheep.y = 220 + Math.floor(Math.random() * (canvas.height - 300));
+					console.log(sheep.y);
+					var sheepX = Math.floor(Math.random() * (2));
+					console.log(sheepX + ' :sheepX');
+					sheepImage.src = "images/sheep/SheepR1.png";
+					if (sheepX == 0){
+						sheep.speed = -sheep.speed;
+						sheepImage.src = "images/sheep/SheepL1.png";
+					}	
+				}
+				if(sheepsCaught == 100){
+					if (wolfFirst){
+						// Throw the wolf somewhere on the screen randomly
+						wolf.x = 30 + Math.floor(Math.random() * (canvas.width - 80));
+						wolf.y = 220 + Math.floor(Math.random() * (canvas.height - 300));
+						console.log(wolf.y);
+						var wolfX = Math.floor(Math.random() * (2));
+						console.log(wolfX + ' :wolfX');
+						wolfImage.src = "images/wolf/WolfR1.png";
+						if (wolfX == 0){
+							wolf.speed = -wolf.speed;
+							wolfImage.src = "images/wolf/WolfL1.png";
+						}
+						wolfFirst = false;
+					}
+				}
+				//}
+			//}
+			
+			first = false;
+			wolfDog = false;
+		}
+	}
+};
 
-	//Update game objects
+// ***************************************Update game objects
 	//Animation looping
 	var imgLoop = 1;
 	//Out of bounds box paramaters
@@ -136,6 +276,7 @@
 	var uBounds = 220;
 	var dBounds = 935;
 	var aboutFace = 1;
+	/*
 	//Timer Alternation for sprite changes
 	function spriteTimer(){
 		if (imgLoop == 1){
@@ -146,10 +287,65 @@
 		}
 		console.log(imgLoop);
 	}
-
+*/
 	var update = function (modifier) {
 	
-	 // Player holding up
+// ***************************************CREDITS CONTROLS FOR SET 0
+	if (controlSet == 0){
+		console.log(controlSet + '&' + gameLevel);
+		// Player press right
+		if (39 in keysDown) {
+			gameLevel = 1;
+			reset();
+			}
+	}
+
+// ***************************************MAIN MENU CONTROLS FOR SET 1
+	if (controlSet == 1){
+		console.log(controlSet + '&' + gameLevel);
+		// Player press left
+		if (37 in keysDown) { 
+			gameLevel = 0;
+			reset();
+			}
+		// Player press right
+		if (39 in keysDown) {
+			gameLevel = 2;
+			reset();
+			}
+		// Player press down
+		if (38 in keysDown) { 
+			gameLevel = 3;
+			menuScreen = false;
+			winLose = false;
+			reset();
+			}
+	}
+
+// ***************************************RULES CONTROLS FOR SET 2
+	if (controlSet == 2){
+		console.log(controlSet + '&' + gameLevel);
+		// Player press left
+		if (37 in keysDown) { 
+			gameLevel = 1;
+			reset();
+			}
+		// Player press right
+		if (39 in keysDown) {
+			gameLevel = 2;
+			reset();
+			}
+		// Player press down
+		if (40 in keysDown) { 
+			gameLevel = 1;
+			reset();
+			}
+	}
+
+// ***************************************GAME PLAY CONTROLS FOR SET 3
+	if (controlSet == 3){
+		//console.log(controlSet + '&' + gameLevel);
+		// Player holding up
 		if (38 in keysDown) {
 			dog.y -= dog.speed * modifier;
 			//aboutFace = 4;
@@ -170,57 +366,49 @@
 		// Player holding left
 		if (37 in keysDown) {
 			dog.x -= dog.speed * modifier;
-			aboutFace = 3;
-			//Light Run State 1
-			if(imgLoop == 1){
-				dogImage.src = "images/dog/DogL2.png";
-				spriteTimer();
-				
-			}
-			//Light Run State 2
-			if(imgLoop == 2){
-				dogImage.src = "images/dog/DogL2.png";
-				spriteTimer();
-			}
+			aboutFace = 1;
+			dogImage.src = "images/dog/DogL2.png";
 			//Prevent left out ouf bounds
 			if (dog.x<=lBounds){
 				dog.x=lBounds;
 			}
 		}
+		else{
+			if (aboutFace == 1){
+			dogImage.src = "images/dog/DogL1.png";
+			}
+		}
 		// Player holding right
 		if (39 in keysDown) { 
 			dog.x += dog.speed * modifier;
-			aboutFace = 1;
-			//Right Run State 1
-			if(imgLoop == 1){
-				dogImage.src = "images/dog/DogR2.png";
-				spriteTimer();
-			}
-			//Right Run State 2
-			if(imgLoop == 2){
-				dogImage.src = "images/dog/DogR2.png";
-				spriteTimer();
-			}
+			aboutFace = 2;
+			dogImage.src = "images/dog/DogR2.png";
+				
 			//Prevent right out ouf bounds
 			if (dog.x>=rBounds){
 				dog.x=rBounds;
 			}
 		}
 		else{
-			if (aboutFace == 1){
-			dogImage.src = "images/dog/DogR1.png";
-			}
 			if (aboutFace == 2){
 				dogImage.src = "images/dog/DogR1.png";
 			}
-			if (aboutFace == 3){
-				dogImage.src = "images/dog/DogL1.png";
-			}
-			if (aboutFace == 4){
-				dogImage.src = "images/dog/DogL1.png";
-			}
 		}
-		
+	}
+	
+// ***************************************WIN LOSE CONTROLS FOR SET 4
+	if (controlSet == 4){
+		console.log(controlSet + '&' + gameLevel);
+		// Player press down
+		if (40 in keysDown) { 
+			gameLevel = 1;
+			menuScreen = true;
+			winLose = false;
+			reset();
+			}
+	}
+	
+// ***************************************CHARACTER MOVEMENT PROPERTIES LOOP
 		//sheep movemenet
 		//Prevent right out ouf bounds
 		if (sheep.x>=rBounds){
@@ -243,9 +431,55 @@
 			&& dog.y <= (sheep.y + 32)
 			&& sheep.y <= (dog.y + 32)
 		) {
-			++sheepsCaught;
+			//Update the points
+			sheepsCaught += 100;
 			reset();
+		};
+		
+		//wolf movemenet
+		//Prevent right out ouf bounds
+		if (wolf.x>=rBounds){
+			wolf.speed = -wolf.speed;
+			wolf.x = (wolf.x - 1);
+			wolfImage.src = "images/wolf/WolfL1.png";
 		}
+		//Prevent Left out ouf bounds
+		if (wolf.x<=lBounds){
+			wolf.speed = -wolf.speed;
+			wolf.x = (wolf.x + 1);
+			wolfImage.src = "images/wolf/WolfR1.png";
+		}
+		wolf.x += wolf.speed * modifier;
+		
+		// Are they touching a wolf?
+		if (
+			dog.x <= (wolf.x + 32)
+			&& wolf.x <= (dog.x + 32)
+			&& dog.y <= (wolf.y + 32)
+			&& wolf.y <= (dog.y + 32)
+		) {
+			//Life counter
+			if (hitCount < 3){
+				hitCount++;
+				wolfDog = true
+			} 
+			reset();
+		};
+		
+		//Did a wolf touch a sheep?
+		if (
+			sheep.x <= (wolf.x + 32)
+			&& wolf.x <= (sheep.x + 32)
+			&& sheep.y <= (wolf.y + 32)
+			&& wolf.y <= (sheep.y + 32)
+		) {
+			//Life counter
+			if (hitCount < 3){
+				hitCount++;
+			} 
+			reset();
+		};
+		
 		/*
 		//Pause and Reset
 		//Pause Game
@@ -269,40 +503,52 @@
 		*/
 	};
 
-	// Draw everything
+//  ***************************************Draw everything
 	var render = function () {
 		if (bgReady) {
 			ctx.drawImage(bgImage, 0, 0);
 		}
 
-		if (dogReady) {
-			ctx.drawImage(dogImage, dog.x, dog.y);
+		if (!menuScreen){
+			if (!winLose){
+				if (dogReady) {
+					ctx.drawImage(dogImage, dog.x, dog.y);
+				}
+				if (sheepReady) {
+					ctx.drawImage(sheepImage, sheep.x, sheep.y);
+				}
+				if (wolfReady) {
+					ctx.drawImage(wolfImage, wolf.x, wolf.y);
+				}
+				
+				// Score
+				ctx.fillStyle = "#5C3317";
+				ctx.font = "30px AR Christy";
+				ctx.fontWeight = "1000";
+				ctx.textAlign = "left";
+				ctx.textBaseline = "top";
+				var xTitle = 85;
+				if (sheepsCaught > 99){
+					xTitle = 70;
+				}
+				if (sheepsCaught > 999){
+					xTitle = 65;
+				}
+				ctx.fillText("BAH-BAH Points: " + sheepsCaught, xTitle, 162);
+				
+				// Lives
+				var lives = ['O O O','O O X','O X X','X X X'];
+				ctx.fillStyle = "#5C3317";
+				ctx.font = "30px AR Christy";
+				ctx.fontWeight = "1000";
+				ctx.textAlign = "left";
+				ctx.textBaseline = "top";
+				ctx.fillText("Lives: " + lives[hitCount], 490, 162);
+			}
 		}
-
-		if (sheepReady) {
-			ctx.drawImage(sheepImage, sheep.x, sheep.y);
-		}
-
-		// Score
-		ctx.fillStyle = "#5C3317";
-		ctx.font = "30px AR Christy";
-		ctx.fontWeight = "1000";
-		ctx.textAlign = "center";
-		ctx.textBaseline = "top";
-		ctx.fillText("BAH-BAH Points: " + sheepsCaught, 195, 157);
-		
-		// Level
-		var season = ['Spring','Fall','Winter'];
-		var s = 0;
-		ctx.fillStyle = "#5C3317";
-		ctx.font = "30px AR Christy";
-		ctx.fontWeight = "1000";
-		ctx.textAlign = "left";
-		ctx.textBaseline = "top";
-		ctx.fillText("Season: " + season[s], 490, 157);
 	};
 
-	// The main game loop
+//  ***************************************The main game loop
 	var main = function () {
 		var now = Date.now();
 		var delta = now - then;
@@ -316,18 +562,13 @@
 
 		// Request to do this again ASAP
 		requestAnimationFrame(main);
-		
-		//If screen is resized
-		window.addEventListener('resize', resizeCanvas, false);
-		window.addEventListener('orientationchange', resizeCanvas, false);
-		resizeCanvas();
 	};
 
-	// Cross-browser support for requestAnimationFrame
+//  ***************************************Cross-browser support for requestAnimationFrame
 	var w = window;
 	requestAnimationFrame = w.requestAnimationFrame || w.webkitRequestAnimationFrame || w.msRequestAnimationFrame || w.mozRequestAnimationFrame;
 
-	// Let's play this game!
-	var then = Date.now();
+//  ***************************************Run and update game
+	var then = Date.now(); 
 	reset();
 	main();
